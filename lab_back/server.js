5b0fede4b27a543d50ec4106d9a763f9acec0a7f
MatiasND0 <matias.nd99@gmailcom>
2024-08-02 13:08:48 -0300

    Login y Register en el back

--- lab_back/server.js
+++ lab_back/server.js
@@ -1,4 +1,15 @@
-var mysql = require('mysql2')
+
+const express = require('express')
+const cors = require('cors');
+const mysql = require('mysql2')
+const bcrypt = require('bcryptjs');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -15,14 +26,6 @@ connection.connect((err) => {
   console.log('Conectado a la base de datos como id ' + connection.threadId);
 });
 
-module.exports = connection;
-
-
-const express = require('express')
-const cors = require('cors');
-
-const app = express();
-const PORT = 3000;
 
 app.use(cors());
 app.use(express.json());
@@ -87,5 +90,56 @@ app.get('/api/libros', (req, res) => {
 
 });
 
+app.post('/register', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  bcrypt.hash(password, 8, (err, hash) => {
+    if (err) {
+      return res.status(500).send('Error al hashear la contraseña');
+    }
+
+    const sql = 'INSERT INTO users (username, password) VALUES (?, ?)';
+    connection.query(sql, [username, hash], (err, result) => {
+      if (err) {
+        return res.status(500).send('Error al registrar el usuario');
+      }
+      res.status(200).send('Usuario registrado correctamente');
+    });
+  });
+});
+
+app.post('/login', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const sql = 'SELECT * FROM users WHERE username = ?';
+  connection.query(sql, [username], (err, results) => {
+    if (err) {
+      return res.status(500).json({ message: 'Error en la consulta a la base de datos' });
+    }
+
+    if (results.length === 0) {
+      return res.status(401).json({ message: 'Usuario no encontrado' });
+    }
+
+    const user = results[0];
+    bcrypt.compare(password, user.password, (err, isMatch) => {
+      if (err) {
+        return res.status(500).json({ message: 'Error al comparar las contraseñas' });
+      }
+
+      if (!isMatch) {
+        return res.status(401).json({ message: 'Contraseña incorrecta' });
+      }
+
+      res.status(200).json({ message: 'Inicio de sesión exitoso' });
+    });
+  });
+});
+
 
 app.listen(PORT, () => console.log(`Servidor escuchando http://localhost:${PORT}`))
+
+
+
